7ca68a56a1e10441ee9620a4607f95d076b3309f
frankNowinski <nowinski.frank@gmail.com>
2016-03-25 02:11:14 -0400

    Done.

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -1,28 +1,31 @@
 function ContactCard() {
-	return {
-		scope: {
-			name: '=',
-			email: '=',
-			phone: '=',
-			username: '='
-		},
-		template: [
-			'<div>',
-				'<h4>Contact Card</h4>',
-				'<label>Name:</label>',
-				'{{ name }}',
-				'<label>Email:</label>',
-				'{{ email }}',
-				'<label>Phone:</label>',
-				'{{ phone }}',
-				'<label>Username:</label>',
-				'<span class="username">{{ username }}</span>',
-			'</div>'
-		].join(''),
-		restrict: 'E'
-	};
+  return {
+    scope: {
+      name: '=',
+      email: '=',
+      phone: '=',
+      username: '='
+    },
+    template: [
+      '<div>',
+        '<h4>Contact Card</h4>',
+        '<label>Name:</label>',
+        '{{ name }}',
+        '<label>Email:</label>',
+        '{{ email }}',
+        '<label>Phone:</label>',
+        '{{ phone }}',
+        '<label>Username:</label>',
+        '<span class="username">{{ username }}</span>',
+      '</div>'
+    ].join(''),
+    restrict: 'E',
+    controller: function($scope){
+      $scope.username = $scope.username.toLowerCase();
+    }
+  };
 }
 
 angular
-	.module('app')
-	.directive('contactCard', ContactCard);
+  .module('app')
+  .directive('contactCard', ContactCard);
